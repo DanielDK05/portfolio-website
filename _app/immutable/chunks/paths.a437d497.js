d35ad306e45ed2a59e71500ab98efc30d8677bf8
DanielDK05 <62153701+DanielDK05@users.noreply.github.com>
2023-12-12 12:39:18 +0100

    Fixed styling errors

--- _app/immutable/chunks/paths.a437d497.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_wptd5p)==null?void 0:s.base)??"/portfolio-website";var t;const a=((t=globalThis.__sveltekit_wptd5p)==null?void 0:t.assets)??e;export{a,e as b};
