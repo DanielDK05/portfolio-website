2adde6915fb97bb115e2a6277e64e96fb21a0331
DanielDK05 <62153701+DanielDK05@users.noreply.github.com>
2023-12-13 11:40:33 +0100

    Added about me

--- _app/immutable/chunks/paths.17b17a34.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_zs2uws)==null?void 0:s.base)??"/portfolio-website";var e;const a=((e=globalThis.__sveltekit_zs2uws)==null?void 0:e.assets)??t;export{a,t as b};
